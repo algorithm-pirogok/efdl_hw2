87933bd46ce95e4250ef1439cbfbd28331baaacb
root <root@LAPTOP-BJSQD76O.localdomain>
2024-02-18 15:13:32 +0300

    Add StaticScaler

--- task1/train.py
+++ task1/train.py
@@ -5,6 +5,27 @@ from tqdm.auto import tqdm
 from unet import Unet
 
 from dataset import get_train_data
+from typing import Literal
+
+class StaticScaler:
+    def __init__(self):
+        self.scale_coeff = 2**6
+    
+    def scale(self, unscale_loss: torch.Tensor):
+        return unscale_loss * self.scale_coeff
+    
+    def step(self, optimizer: torch.optim.Optimizer):
+        for group in optimizer.param_groups:
+            for param in group['params']:
+                if param.grad is not None:
+                    param.grad = param.grad / self.scale_coeff
+                    if not torch.isfinite(param.grad).all().item():
+                        self._update()
+        optimizer.step()
+                    
+    
+    def _update(self):
+        pass
 
 
 def train_epoch(
@@ -13,9 +34,17 @@ def train_epoch(
     criterion: torch.nn.modules.loss._Loss,
     optimizer: torch.optim.Optimizer,
     device: torch.device,
+    mode_of_precision: Literal["base", "static", "dynamic"] = "static"
 ) -> None:
     model.train()
 
+    if mode_of_precision == "static":
+        scaler = StaticScaler()
+    elif mode_of_precision == "dynamic":
+        pass
+    elif mode_of_precision == "base":
+        scaler = None
+    
     pbar = tqdm(enumerate(train_loader), total=len(train_loader))
     for i, (images, labels) in pbar:
         images = images.to(device)
@@ -24,8 +53,12 @@ def train_epoch(
         with torch.cuda.amp.autocast():
             outputs = model(images)
             loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            if scaler is not None:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+            else:
+                loss.backward()
+                optimizer.step()
             optimizer.zero_grad()
         # TODO: your code for loss scaling here
 
@@ -34,7 +67,7 @@ def train_epoch(
         pbar.set_description(f"Loss: {round(loss.item(), 4)} " f"Accuracy: {round(accuracy.item() * 100, 4)}")
 
 
-def train():
+def train(mode_of_precision):
     device = torch.device("cuda:0")
     model = Unet().to(device)
     criterion = nn.BCEWithLogitsLoss()
@@ -44,7 +77,7 @@ def train():
 
     num_epochs = 5
     for epoch in range(0, num_epochs):
-        train_epoch(train_loader, model, criterion, optimizer, device=device)
+        train_epoch(train_loader, model, criterion, optimizer, device=device, mode_of_precision=mode_of_precision)
 
 if __name__ == '__main__':
-    train()
+    train(mode_of_precision="static")
