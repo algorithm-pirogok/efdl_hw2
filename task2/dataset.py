5fddff9fb26373dc8184ad10ee76f98e482c9641
root <root@LAPTOP-BJSQD76O.localdomain>
2024-02-18 19:10:40 +0300

    Add task2

---
+++ task2/dataset.py
@@ -0,0 +1,92 @@
+import os
+from typing import Optional
+
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import Sampler
+import torchtext
+
+MAX_LENGTH = 640
+max_tokens = 5000
+
+    
+def load_dataset(data_path):
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+        
+        print("LOAD DATASET")
+        dataset = load_dataset("wikitext", "wikitext-103-raw-v1", split="train", ignore_verifications=True)["text"]
+        
+        print("CREATE TOKENS")
+        tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+        tokens = [tokenizer(elem) for elem in dataset]
+        
+        print("CREATE VOCABULARY")
+        vocabulary = torchtext.vocab.build_vocab_from_iterator(
+            iter(tokens), max_tokens=max_tokens, mspecials=["<unk>"]
+        )
+        vocabulary.set_default_index(vocabulary["<unk>"])
+        torch.save(vocabulary, os.path.join(data_path, "vocabulary.pt"))
+        
+        print("FILTER")
+        filter_data = [torch.tensor(vocabulary(elem)).astype(int) for elem in dataset if len(vocabulary(elem))]
+        torch.save(filter_data, os.path.join(data_path, "dataset.pt"))
+        torch.save(filter_data[:10000], os.path.join(data_path, "dataset_small.pt"))
+        
+
+class BrainDataset(Dataset):
+    def __init__(self, data_path, max_length: int = MAX_LENGTH):
+        self.dataset = load_dataset(data_path)
+        self.max_length = max_length
+        
+    def __getitem__(self, idx: int):
+        return self.dataset[idx]
+
+
+class BigBrainDataset(Dataset):
+    def __init__(self, data_path, max_length: int = MAX_LENGTH):
+        self.dataset = load_dataset(data_path)
+        self.max_length = max_length
+
+    def __getitem__(self, idx: int):
+        return self.dataset[idx]
+
+
+class UltraDuperBigBrainDataset(Dataset):
+    def __init__(self, data_path: str, max_length: int = MAX_LENGTH, n_bins: int = 1):
+        pass
+
+    def __getitem__(self, idx: int):
+        pass
+
+
+def collate_fn(
+    batch: list[torch.Tensor], max_length: Optional[int] = MAX_LENGTH
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    Pad each sequence of the incoming sequences list
+    :param batch: a list of the objects received from the dataset by __getitem__
+    :param max_length: maximum sequence length to pad to (for "Brain" approach only)
+    :return: tuple of padded sequences and corresponding training targets
+    """
+    if max_length is None:
+        max_length = max([elem.shape[0] for elem in batch])
+    answer = torch.zeros((batch.shape[0], max_length), dtype=batch[0].dtype)
+    target = torch.zeros((batch.shape[0], max_length), dtype=batch[0].dtype)
+    for ind, elem in enumerate(batch):
+        fix_elem = elem[:max_length]
+        fix_len = fix_elem.shape[0]
+        answer[ind][:fix_len], target[ind][:fix_len] = fix_elem, 1
+    return answer, target
+
+
+class UltraDuperBigBrainBatchSampler(Sampler):
+
+    def __init__(self, batch_size: int, max_length: Optional[int] = MAX_LENGTH):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
---
+++ task2/run_epoch.py
@@ -0,0 +1,24 @@
+from enum import Enum
+import os
+
+
+import torch
+from dataset import load_dataset
+
+DATASET_PATH = os.getcwd() / "data"
+
+class DataMode(Enum):
+    BRAIN = 1
+    BIG_BRAIN = 2
+    ULTRA_DUPER_BIG_BRAIN = 3
+
+
+def get_gpt2_model() -> torch.nn.Module:
+    pass
+
+
+def run_epoch(data_mode: DataMode) -> None:
+    pass
+
+
+load_dataset(DATASET_PATH)
