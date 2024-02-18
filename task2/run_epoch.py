4be47da500f1cf92ded8da6310a10cc250fc8430
root <root@LAPTOP-BJSQD76O.localdomain>
2024-02-18 21:44:05 +0300

    Fix

--- task2/run_epoch.py
+++ task2/run_epoch.py
@@ -75,4 +75,4 @@ def run_epoch(data_mode: DataMode) -> None:
     print(f"Mode: {mode}\nmin: {np.min(lst)}\nmean: {np.mean(lst)}\nmax: {np.max(lst)}\nmedian: {np.median(lst)}")
     
 if __name__ == "__main__":
-    run_epoch(DataMode.BIG_BRAIN)
+    run_epoch(DataMode.BRAIN)
